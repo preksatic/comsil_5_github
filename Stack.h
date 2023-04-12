ddce9726bbb3e481a0fdb642155491d5115457bd
preksatic <79179032+preksatic@users.noreply.github.com>
2023-04-12 12:27:56 +0900

    3

--- Stack.h
+++ Stack.h
@@ -1,6 +1,5 @@
 #include "LinkedList.h"
 
-
 //1. 템플릿 클래스로 확장해야함
 //2. Stack형식으로 Delete 함수 재정의해야함
 //주의: first, current_size는 class의 멤버 변수이기 때문에 this 포인터를 사용하여 가져와야함
@@ -19,7 +18,7 @@ class Stack : public LinkedList<T>{
                         }
 			// LinkedList와 달리 Stack은 current가 가리키는 곳을 삭제
                            
-
+			int i = 2;
 			return true;
 			}
 };
